9b2f8ef4297a82010270b9e4d81971bbc21baf13
MrEngineer <freekick20.com@gmail.com>
2024-05-16 15:10:51 +0330

    comment on timeSlider component

--- exam/src/TimeSlider.tsx
+++ exam/src/TimeSlider.tsx
@@ -1,7 +1,7 @@
 import { Slider } from "@mui/material";
 import React from "react";
 
-//const timerValue
+//you can develope this option for your app
 setInterval(() => {});
 export const TimeSlider: React.FC<{}> = ({}): React.ReactNode => {
   return <Slider max={300} min={0} size="small" disabled></Slider>;
